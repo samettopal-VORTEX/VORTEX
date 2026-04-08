b927874df1ab44a419578bbdbd7c377a039cdab4
samettopal-VORTEX <seyfettinsamettopal@gmail.com>
2026-04-08 19:24:03 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+def vortex_init():
+    print("--- V.O.R.T.E.X. System Initializing ---")
+    print("Status: Active")
+    print("Owner: samettopal-VORTEX")
+    print("---------------------------------------")
+
+if __name__ == "__main__":
+    vortex_init()
